132c321c3a64c02b912bf2fe00442fe52f174e75
Philipp Rich <skorphil@gmail.com>
2025-10-14 22:01:30 +0400

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+const { rules } = require('eslint-plugin-react-refresh');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb', 'airbnb-typescript', 'plugin:react/jsx-runtime'],
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    '*/**.css',
+  ],
+  overrides: [
+    {
+      // Allow mocking in tests
+      files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+      rules: {
+        'import/first': 'off',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.app.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'object-curly-newline': 'off',
+    'implicit-arrow-linebreak': 'off', // conflicting with limit characters in a line
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'react/require-default-props': 'off', // to prevent conflicts with functional React Components
+    // 'max-lines-per-function': ['error', 40],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state'], // Allow mutations within redux Toolkit
+      },
+    ],
+  },
+};
