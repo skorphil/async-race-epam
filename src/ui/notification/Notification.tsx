288adf3ea25abd06aef19949f7c162374c0287f0
Philipp Rich <skorphil@gmail.com>
2025-10-23 12:06:41 +0400

    refactor: changed winner notification position to center in response to: There’s also an issue with the modal view — it would be better if we could move it to the center

--- src/ui/notification/Notification.tsx
+++ src/ui/notification/Notification.tsx
@@ -1,11 +1,15 @@
 // src/ui/notification/Notification.tsx
 import ReactDOM from 'react-dom';
-import { useSelector } from 'react-redux';
-import type { RootState } from '@/store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { XIcon } from 'lucide-react';
+import type { AppDispatch, RootState } from '@/store/store';
 import styles from './Notification.module.css';
+import raceSlice from '@/store/raceSlice';
 
 function Notification() {
   const notification = useSelector((state: RootState) => state.race.winner);
+  const dispatch = useDispatch<AppDispatch>();
+  const { setWinner } = raceSlice.actions;
 
   const notificationRoot = document.getElementById('notification-root');
   if (!notificationRoot) return null;
@@ -13,8 +17,15 @@ function Notification() {
     return null;
   }
 
+  const handleClose = () => {
+    dispatch(setWinner(undefined));
+  };
+
   return ReactDOM.createPortal(
     <article className={styles.container}>
+      <button type="button" className="outline secondary" onClick={handleClose}>
+        <XIcon />
+      </button>
       <h2>Winner</h2>
       <p>{notification.name}</p>
       <p>{notification.time}</p>
